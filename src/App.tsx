03092825128ad34e43454cb66ce648fdd43e9257
gizemdusmez <dusmezgizem11@gmail.com>
2026-02-27 17:49:30 +0300

    chore: initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarımı ve Programlama</h1>
+      <h2>LAB - 1</h2>
+      <p>Ad Soyad: Gizem Düşmez</p>
+      <p>Öğrenci No: XXXXX</p>
+      <p>Bölüm: Yazılım Mühendisliği</p>
+      <p>Hobilerim: Kod yazmak, spor, müzik</p>
+    </div>
+  )
+}
+export default App
